277a86d512bb4f5250aa93ae65075ca63315e44c
Yusufbld <bulduyusuf49@gmail.com>
2023-11-27 19:56:34 +0100

    tag9

---
+++ main.js
@@ -0,0 +1,12 @@
+// alert ruft ein fenster auf
+alert("hallo jannick");
+
+/*
+-hallo
+-das hier ist ein kommentar
+*/
+
+console.log("ich bin in der konsole")
+console.error
+console.warn("2+2")
+
---
+++ variable.js
@@ -0,0 +1,28 @@
+// strings
+let name="jannick";
+let surname= "leismann"
+
+//integer
+let age = 18;
+
+//float
+let price = 3.99;
+
+//boolean
+let hasdrivinglicense =true;
+let iswaiting =false;
+
+// letzte änderung zählt
+let x= 5;
+x =10;
+x = 20;
+let y=-10;
+
+let result=x+y;
+
+console.log(result);
+
+
+ x = x-2;
+
+console.log( x == 18);
