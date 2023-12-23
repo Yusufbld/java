2e5d05142c63a3861ae584e0c659e2ed4daf3993
Yusufbld <bulduyusuf49@gmail.com>
2023-12-23 17:04:23 +0100

    tag11

--- Modul6/8_if_statements_Blitzer.js
+++ Modul6/8_if_statements_Blitzer.js
@@ -1,16 +1,22 @@
 // if statements
 
 function ifStatementsBlitzer(){
-
-    var inputSpeed = document.getElementById("speed");
-    var inputdir = document.getElementById("dir");
     
+
+    var inputMaxSpeed = document.getElementById("maxSpeed");
+    var inputSpeed = document.getElementById("speed");
+    var inputDir = document.getElementById("dir");
+    
+    var maxSpeed = inputMaxSpeed.value;
     var speed = inputSpeed.value;
     var isFacing = inputDir.checked;
 
-    console.log(speed)
+var maxSpeedWithOffset = maxSpeed + ((maxSpeed/100)*10);
+console.log(maxSpeedWithOffset);
 
-    console.log(isFacing)
+if(speed > maxSpeed && isFacing == true);{
+        alert("du wurdest geblitzt");
+    }
 
 
 
